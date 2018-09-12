27b15a05dfab789fb5877913ef60650f3bccdc39
WildZes <wildzes@gmail.com>
2018-09-12 23:23:40 +0300

    Изменил высоту рабочего окна

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("JavaFX project Client");
-        primaryStage.setScene(new Scene(root, 550, 800));
+        primaryStage.setScene(new Scene(root, 550, 500));
         primaryStage.show();
     }
 
