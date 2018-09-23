153dc0fe7ddb85c8c8ad00904cd606c524b84c03
Aleksandr Jefimov <jefimov33@yandex.com>
2018-09-23 18:49:15 +0300

    создал класс

--- src/main/java/client/SocketThreadListener.java
+++ src/main/java/client/SocketThreadListener.java
@@ -1,4 +1,18 @@
 package client;
 
-public class SocketThreadListener {
+import server.SocketThread;
+
+import java.net.Socket;
+
+public interface SocketThreadListener {
+    void onStartSocketThread(SocketThread socketThread, Socket socket);
+
+    void onStopSocketThread(SocketThread thread, Socket socket);
+
+    void onSocketIsReady(SocketThread socketThread, Socket socket, Object ChatServer);
+
+    void onSocketIsReady(SocketThread thread, Socket socket);
+
+    void onReceivedString(SocketThread socketThread, Socket socket, String msg);
+
 }
