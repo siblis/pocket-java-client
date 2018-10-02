ec1c0ed7435ffb8493e492f553e5449d03ac60e1
OzzyFrost <5kla@mail.ru>
2018-10-02 11:48:07 +0500

    оптимизировал код HTTPSRequest

--- src/main/java/client/Connector.java
+++ src/main/java/client/Connector.java
@@ -1,7 +1,5 @@
 package client;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
--- src/main/java/client/HTTPSRequest.java
+++ src/main/java/client/HTTPSRequest.java
@@ -1,18 +1,14 @@
 package client;
 
-import com.sun.net.httpserver.HttpsServer;
-import jdk.nashorn.internal.parser.JSONParser;
-
-import java.net.ResponseCache;
 import java.net.URL;
 import java.io.*;
 import javax.net.ssl.HttpsURLConnection;
 
 class HTTPSRequest {
+    private static final String serverURL = "https://pocketmsg.ru:8888";
 
     static int registration(String requestJSON) throws Exception {
-        String url = "https://pocketmsg.ru:8888/v1/users/";
-        URL obj = new URL(url);
+        URL obj = new URL(serverURL+"/v1/users/");
         HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
         con.setRequestMethod("POST");
 
@@ -25,12 +21,10 @@ class HTTPSRequest {
     }
 
     static String avtorization(String requestJSON) throws Exception {
-        String url = "https://pocketmsg.ru:8888/v1/auth/";
-        URL obj = new URL(url);
+        URL obj = new URL(serverURL+"/v1/auth/");
         HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
         con.setRequestMethod("PUT");
         sendRequest(con,requestJSON);
-
         return answerRequest(con);
     }
 
@@ -66,30 +60,12 @@ class HTTPSRequest {
         return response.toString();
     }
     public static void addContact (String requestJSON, String token)throws Exception{
-
-        String url = "https://pocketmsg.ru:8888/v1/users/";
-        URL obj = new URL(url);
+        URL obj = new URL(serverURL+"/v1/users/");
         HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
         con.setRequestMethod("PUT");
         con.setRequestProperty("Token", token);
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(requestJSON);
-        wr.flush();
-        wr.close();
 
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'PUT' request to URL : " + url);
-        System.out.println("PUT parameters : " + requestJSON );
-        System.out.println("Response Code : " + responseCode);
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        System.out.println(response.toString());
+        sendRequest(con,requestJSON);
+        answerRequest(con);
     }
 }
