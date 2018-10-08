022af39e27b44e66dc70f6cc3eb5b0fd192ebbda
OzzyFrost <5kla@mail.ru>
2018-10-08 06:00:17 +0500

    теперь можно переписываться с любым, над лишь нажать на его ник в листе контактов

--- src/main/java/client/Controller.java
+++ src/main/java/client/Controller.java
@@ -56,6 +56,7 @@ public class Controller implements Initializable {
     private WebView webView = null;
 
     private String myNick;
+    private String receiver = "24";
 
     private Connector conn = null;
 
@@ -135,7 +136,6 @@ public class Controller implements Initializable {
             if (answer.contains("token")) {
                 //тут надо обработать JSON по нармальному
                 token = answer.substring(answer.indexOf("token") + 9, answer.indexOf(",") - 1);
-                System.out.println("TOKEN " + token);
                 setAutorized(true);
                 connect(token);
                 myNick = loginField.getText();
@@ -150,7 +150,7 @@ public class Controller implements Initializable {
         Date dateNow = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-        String receiver = myNick.equals("tester2") ? "25" : "24";
+//        String receiver = myNick.equals("tester2") ? "25" : "24";
         String mess = "{ \"receiver\":\"" +
                 receiver +
                 "\", \"message\":\"" +
@@ -178,8 +178,11 @@ public class Controller implements Initializable {
     }
 
     public void clientChoice(MouseEvent event) {
-        if (event.getClickCount() == 2) {
-            msgField.setText("/w " + clientsListArea.getSelectionModel().getSelectedItem() + " ");
+        if (event.getClickCount() == 1) {
+//            msgField.setText("/w " + clientsListArea.getSelectionModel().getSelectedItem() + " ");
+            receiver = clientsListArea.getSelectionModel().getSelectedItem();
+            showAlert("Сообщения будут отправляться контакту \n"
+                    +receiver,"Временное решение");
             msgField.requestFocus();
             msgField.selectEnd();
         }
@@ -213,6 +216,7 @@ public class Controller implements Initializable {
         loginField.setText("tester2");
         passFiead.setText("123");
         authentication();
+        receiver ="25";
     }
 
     public void conn3() {
@@ -221,6 +225,7 @@ public class Controller implements Initializable {
         loginField.setText("tester3");
         passFiead.setText("123");
         authentication();
+        receiver ="24";
     }
 
     public void exit() {
@@ -257,9 +262,9 @@ public class Controller implements Initializable {
             int userId = HTTPSRequest.addContact(requestJSON, token);
             if (userId != -1) {
                 addToList(userId);
-            }else{
-                showAlert("Пользователь с email: "+addContact.getText()+
-                        " не найден","Ошибка добавления контакта");
+            } else {
+                showAlert("Пользователь с email: " + addContact.getText() +
+                        " не найден", "Ошибка добавления контакта");
             }
 
         } catch (Exception e) {
@@ -272,9 +277,9 @@ public class Controller implements Initializable {
 //         в дальнейшем будет добавлен User , а не id юзера
         if (!clientsObsvList.contains(id)) {
             clientsObsvList.add(id);
-            showAlert("Контакт "+id+" успешно добавлен","Добавление контакта");
-        }else{
-            showAlert("Пользователь "+ id+" уже есть в списке ваших контактов","Ошибка добавления контакта");
+            showAlert("Контакт " + id + " успешно добавлен", "Добавление контакта");
+        } else {
+            showAlert("Пользователь " + id + " уже есть в списке ваших контактов", "Ошибка добавления контакта");
         }
     }
 
