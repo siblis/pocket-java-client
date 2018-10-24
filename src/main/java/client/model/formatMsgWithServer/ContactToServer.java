ab2cc3d4de7e2088deadd749e064d1a4d73eaa3c
Stormcoder <teryaevs@yandex.ru>
2018-10-24 23:54:42 +1100

    контакт для преобразования в JSON

---
+++ src/main/java/client/model/formatMsgWithServer/ContactToServer.java
@@ -0,0 +1,9 @@
+package client.model.formatMsgWithServer;
+
+public class ContactToServer {
+    private String contact;
+
+    public ContactToServer(String contact) {
+        this.contact = contact;
+    }
+}
