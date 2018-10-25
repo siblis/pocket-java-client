84dab0f5dae2eaf99c2867e2caf45455e1c7f8f8
manunia <timoshenkomaria@mail.ru>
2018-10-25 12:36:34 +0300

    Cancel сделан обратно кнопкой

--- src/main/java/client/view/RegisterViewController.java
+++ src/main/java/client/view/RegisterViewController.java
@@ -32,7 +32,7 @@ public class RegisterViewController implements Initializable {
     private Button okRegisterButton;
 
     @FXML
-    private Label cancelRegisterButton;
+    private Button cancelRegisterButton;
 
     private ClientController controller;
 
