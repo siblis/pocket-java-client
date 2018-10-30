d7bcbaa439366c8a3c6a884e1c955498ec9eef1c
manunia <timoshenkomaria@mail.ru>
2018-10-30 13:35:15 +0300

    установлена иконка приложения

--- src/main/java/client/view/LogonViewController.java
+++ src/main/java/client/view/LogonViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -87,6 +88,7 @@ public class LogonViewController implements Initializable {
         Stage stage = new Stage();
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.setTitle("New account registration");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/client/images/icon.png")));
         stage.setResizable(false);
         stage.setScene(new Scene(root));
         stage.show();
