d4d68b767533bd45c2987d5636aad2d195aba78f
rgajdov <rgajdov@gmail.com>
2018-10-31 17:33:37 +0300

    Добавлен парсер сообщения на предмет поиска в нем урлов и преобразования в гиперссылки для WebView.

--- src/main/java/client/utils/Common.java
+++ src/main/java/client/utils/Common.java
@@ -2,6 +2,8 @@ package client.utils;
 
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Common {
 
@@ -27,5 +29,18 @@ public class Common {
             alert.setContentText(message);
             alert.showAndWait();
         });
+    }
+
+    public static String urlToHyperlink(String inputString) {
+        String s = "(?i)(?:(?:https?|ftp)://)?(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))\\.?)(?::\\d{2,5})?(?:[/?#]\\S*)?";
+        StringBuffer resultString = new StringBuffer();
+        Pattern regex = Pattern.compile(s);
+        Matcher m = regex.matcher(inputString);
+        while (m.find()) {
+            String replacement = m.group(0).replaceAll(m.group(0), "<a href=\"" + m.group(0) + "\">" + m.group(0) + "</a>");
+            m.appendReplacement(resultString, replacement);
+        }
+        m.appendTail(resultString);
+        return resultString.toString();
     }
 }
