67462c8e0ff52b73e9a37f125404bc7adf691bea
manunia <timoshenkomaria@mail.ru>
2018-11-08 15:42:07 +0300

    добавлен фон webView

--- src/main/java/client/view/ChatViewController.java
+++ src/main/java/client/view/ChatViewController.java
@@ -14,6 +14,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -26,6 +27,8 @@ import java.util.ResourceBundle;
 import static client.utils.Common.showAlert;
 
 public class ChatViewController implements Initializable {
+
+    public WebEngine webEngine;
 
     @FXML
     private AnchorPane messagePanel;
@@ -69,9 +72,25 @@ public class ChatViewController implements Initializable {
     }
 
     private void webtest() {
-        clientController.webEngine = messageWebView.getEngine();
-        clientController.webEngine.setJavaScriptEnabled(true);
+        webEngine = messageWebView.getEngine();
+        webEngine.setJavaScriptEnabled(true);
         clientController.getInstance();
+        webEngine.loadContent("<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+
+                "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n" +
+                "</head>\n" +
+
+                "    <body style=\"background-image: url(https://s3-alpha.figma.com/img/0f65/df21/9351ace9280e6668b235304d7ceaf426)\">\n" +
+
+                "        <div id=\"messageArea\">" +
+                "       </div>\n" +
+
+                "    </body>\n" +
+                "</html>");
+
+
     }
 
     public void fillContactListView() {
