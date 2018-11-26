0f89afd6d7c2d9a4e76da553af10e2cc45197e10
Stormcoder <teryaevs@yandex.ru>
2018-11-26 21:58:20 +1100

    каждому пользователю по базе

--- src/main/java/database/HibernateUtil.java
+++ src/main/java/database/HibernateUtil.java
@@ -5,24 +5,32 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-   private static StandardServiceRegistry registry;
-   private static SessionFactory sessionFactory;
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
+    private static String userName = "defaultUser";
 
-   public static SessionFactory getSessionFactory(){
-       if (sessionFactory == null){
-           registry = new StandardServiceRegistryBuilder().configure().build();
-           MetadataSources sources = new MetadataSources(registry);
-           Metadata metadata = sources.getMetadataBuilder().build();
-           sessionFactory = metadata.getSessionFactoryBuilder().build();
-       }
+    public static void setUserName(String Name) {
+        userName = Name;
+    }
 
-       return sessionFactory;
-   }
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.connection.url",
+                    "jdbc:sqlite:" + userName + ".db");
+            registry = new StandardServiceRegistryBuilder().applySettings(
+                    configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(registry);
+        }
 
-   public static void shutdown(){
-       if(registry != null)
-           StandardServiceRegistryBuilder.destroy(registry);
-   }
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (registry != null)
+            StandardServiceRegistryBuilder.destroy(registry);
+    }
 }
