ec7b82672041dd418c111b6e4313c6ded449e1ed
Shepico <Shepico@gmail.com>
2018-12-19 13:46:38 +0400

    Delete folder HTML --> not use

--- src/main/resources/client/html/EngineMsg.js
+++
@@ -1,9 +0,0 @@
-
-function sendText() {
-    var mes = document.getElementById("messageTextArea").value;
-
-    var newDiv = document.createElement('div');
-    document.getElementById("messageArea").appendChild(newDiv);
-    newDiv.innerHTML = mes;
-}
-
