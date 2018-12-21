01758de5848887149b8ebb562b57ec0bc679ae96
Shepico <Shepico@gmail.com>
2018-12-21 11:29:47 +0400

    Запрет открытия ссылки в WebView добавил.

--- src/main/java/client/view/ChatViewController.java
+++ src/main/java/client/view/ChatViewController.java
@@ -441,7 +441,7 @@ public class ChatViewController implements Initializable {
                         //if (desktop.isSupported(Desktop.Action.BROWSE)) {
                             desktop.browse(new URI(href.contains("://") ? href : "http://" + href + "/"));
                             //отменяем событие, чтобы ссылка не открывалась в самом webView
-                            //evt.preventDefault();
+                            evt.preventDefault();
                         /*} else {
                             System.out.println("Could not load URL: " + href);
                         }*/
