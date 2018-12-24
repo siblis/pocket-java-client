2f4e5704bc96930b1a5c00c097b69466386d33da
PaLoMaster <renat.programmizm@yandex.ru>
2018-12-24 22:59:53 +0400

    Контакт с сервера под новое АПИ

--- src/main/java/client/model/formatMsgWithServer/ContactListFromServer.java
+++ src/main/java/client/model/formatMsgWithServer/ContactListFromServer.java
@@ -1,24 +1,42 @@
 package client.model.formatMsgWithServer;
 
 public class ContactListFromServer {
-    private long id;
-    private String name;
+    private long user_id;
+    private String account_name;
+    private String email;
+    private String status;
 
     public ContactListFromServer() {}
 
     public long getId() {
-        return id;
+        return user_id;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setId(long user_id) {
+        this.user_id = user_id;
     }
 
     public String getName() {
-        return name;
+        return account_name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.account_name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
