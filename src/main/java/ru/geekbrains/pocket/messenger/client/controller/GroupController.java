6bf7c9682f65241ea45422a75fe5ddc95fcb9a9f
zundarik <zundarik@gmail.com>
2019-03-30 19:51:28 +1100

    add new class for refactoring

---
+++ src/main/java/ru/geekbrains/pocket/messenger/client/controller/GroupController.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.pocket.messenger.client.controller;
+
+//for api:
+//Group
+//          /groups/%id
+//          /groups
+//Group members
+//          /groups/%id/members
+//          /groups/%id/members/%id
+//Group invites
+//          /groups/%id/invites
+
+public class GroupController {
+}
---
+++ src/main/java/ru/geekbrains/pocket/messenger/client/controller/MessageController.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.pocket.messenger.client.controller;
+
+//for api:
+//User messages
+//          /user/%id/messages
+//          /user/%id/messages/%id
+//Groups messages
+//          /groups/%id/messages
+//          /groups/%id/messages/%id
+
+public class MessageController {
+}
