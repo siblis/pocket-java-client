547bdf4852f551c22efb2c1c477f17ce24ad51ab
yajaneya <janeya@yandex.ru>
2019-03-31 21:57:25 +0300

    Заменил группу параметров типа String на один параметр Message mess в методах ChatViewController.updateLastMessageInCardsBody и ChatViewController.createMessageDiv

--- src/main/java/client/controller/ClientController.java
+++ src/main/java/client/controller/ClientController.java
@@ -179,8 +179,7 @@ public class ClientController {
         //Как будет с группами пока не понятно
         if (receiver.getUid() == mfs.getSenderid()) {
             Message mess = new Message();
-            UserDAO ud = new UserDAO();
-            mess.setSender(ud.get(mfs.getSenderid()));
+            mess.setSender(dbService.getUser(mfs.getSenderid()));
             mess.setText(mfs.getMessage());
             mess.setTime(mfs.getTimestamp());
             chatViewController.showMessage(mess, true);
