63b7c8382f1ae0c81e4ca745a9fab6b280a89b28
yajaneya <janeya@yandex.ru>
2019-04-20 23:36:07 +0300

    Добавлены проверки на наличие пользователя в начале и отсутствие его в конце, и соотвтетсвующие действия.

--- src/main/java/tests/testUserDB.java
+++ src/main/java/tests/testUserDB.java
@@ -1,9 +1,10 @@
-package ru.geekbrains.pocket.messenger.database.tests;
+package tests;
 
 
-import ru.geekbrains.pocket.messenger.database.dao.DataBaseService;
 import org.junit.*;
-import ru.geekbrains.pocket.messenger.database.entity.*;
+import ru.geekbrains.pocket.messenger.database.dao.DataBaseService;
+import ru.geekbrains.pocket.messenger.database.entity.User;
+import ru.geekbrains.pocket.messenger.database.entity.UserProfile;
 
 import java.sql.Timestamp;
 
@@ -12,23 +13,39 @@ public class testUserDB {
     private DataBaseService dbs = new DataBaseService();
     private UserProfile up;
     private User user;
+    String Id;
 
     @Before
     public void start() {
-        up = new UserProfile("111", "User", "Userov", new Timestamp(0l));
+        Integer id = 111;
+        Id = id.toString();
+        User w = dbs.getUserById(Id);
+        while (!(w==null)) {
+            Id = (++id).toString();
+            w = dbs.getUserById(Id);
+        }
+        up = new UserProfile(Id, "User", "Userov", new Timestamp(0l));
         user = new User("u@u.uu", up);
     }
 
     @Test
     public void testAddUser() {
         dbs.insertUser(user);
-        Assert.assertEquals(user, dbs.getUserById("111"));
+        Assert.assertEquals(user, dbs.getUserById(Id));
+        dbs.deleteUser(user);
     }
 
     @Test
     public void testDelUser() {
+        dbs.insertUser(user);
         dbs.deleteUser(user);
-        Assert.assertNotEquals(user, dbs.getUserById("111"));
-}
+        Assert.assertNotEquals(user, dbs.getUserById(Id));
+    }
+
+    @After
+    public void finish() {
+        if (!(dbs.getUserById(Id)==null))
+            dbs.deleteUser(user);
+    }
 
 }
