3f67512361dbb639d35ed8da2f0e5b9b975dc9a7
Vladim1r37 <vl4d1m1r37@gmail.com>
2019-04-27 23:23:36 +0400

    addition to DeBug-014 commit

--- src/main/java/ru/geekbrains/pocket/messenger/client/utils/HTTPSRequest.java
+++ src/main/java/ru/geekbrains/pocket/messenger/client/utils/HTTPSRequest.java
@@ -10,6 +10,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 
 public class HTTPSRequest {
@@ -40,7 +41,7 @@ public class HTTPSRequest {
     public static String getResponse() throws Exception {
         StringBuilder response = new StringBuilder();
         try (BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()))) {
+                new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
 
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
