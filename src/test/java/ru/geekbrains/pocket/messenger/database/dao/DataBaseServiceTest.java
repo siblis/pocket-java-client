57cee812c5475c4db68b963365299cf542d59947
yajaneya <janeya@yandex.ru>
2019-05-09 15:28:01 +0300

    Добавил тест метода по добавлению сообщений в БД: public void addMessage(Message message) из class DataBaseService

--- src/test/java/ru/geekbrains/pocket/messenger/database/dao/DataBaseServiceTest.java
+++ src/test/java/ru/geekbrains/pocket/messenger/database/dao/DataBaseServiceTest.java
@@ -3,30 +3,28 @@ package ru.geekbrains.pocket.messenger.database.dao;
 import org.junit.*;
 
 import ru.geekbrains.pocket.messenger.database.HibernateUtil;
-import ru.geekbrains.pocket.messenger.database.entity.User;
-import ru.geekbrains.pocket.messenger.database.entity.UserProfile;
+import ru.geekbrains.pocket.messenger.database.entity.*;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public class DataBaseServiceTest {
 
     private DataBaseService dbs = new DataBaseService();
-    private UserProfile up;
     private User user;
     private String Id;
 
     @Before
     public void setUp() throws Exception {
+        dbs.setUserDB("t"); // создается тестовая БД с именем "t.db", т.к. имя пользователя не может состоять из 1 символа.
         Integer id = 111;
         Id = id.toString();
-        dbs.setUserDB("t"); // создается тестовая БД с именем "t.db", т.к. имя пользователя не может состоять из 1 символа.
         User w = dbs.getUserById(Id);
         while (!(w==null)) {
             Id = (++id).toString();
             w = dbs.getUserById(Id);
         }
-        up = new UserProfile(Id, "U", "U", new Timestamp(0l));
-        user = new User("u@u.uu", up);
+        user = new User("u@u.uu", new UserProfile(Id, "U", "U", new Timestamp(0l)));
     }
 
     @After
@@ -35,6 +33,7 @@ public class DataBaseServiceTest {
     }
 
     @Test
+//    @Ignore
     public void insertUser() {
         Assert.assertNull( dbs.getUserById(Id));
         dbs.insertUser(user);
@@ -43,10 +42,49 @@ public class DataBaseServiceTest {
     }
 
     @Test
+//    @Ignore
     public void deleteUser() {
         dbs.insertUser(user);
         Assert.assertEquals(user, dbs.getUserById(Id));
         dbs.deleteUser(user);
         Assert.assertNull(dbs.getUserById(Id));
+    }
+
+    @Test
+    public void addMessage() {
+        dbs.insertUser(user);
+        User userComm = new User("q@q.qq", new UserProfile("222", "His", "His", new Timestamp(01)));
+        dbs.insertUser(userComm);
+
+
+        Assert.assertTrue(okMessageInDB(user, userComm, "Тест", "1"));
+        Assert.assertTrue(okMessageInDB(user, userComm, "Ок!", "2"));
+        Assert.assertTrue(okMessageInDB(user, userComm, "", "3"));
+        Assert.assertTrue(okMessageInDB(user, userComm, "wefrkncjerEf egtrg wgRW trhg vrbgrwbth rgt...", "4"));
+        Assert.assertTrue(okMessageInDB(user, userComm, "Привет, друг! Как дела? ты где сейчас?", "5"));
+        Assert.assertTrue(okMessageInDB(userComm, user,"И тебе привет! Всё отлично!!! Я тут,  а ты где?", "6"));
+        Assert.assertTrue(okMessageInDB(user, userComm,"И я тут... ", "7"));
+
+
+    }
+
+    private Boolean okMessageInDB(User user1, User user2, String mess, String id) {
+        Message message = new Message();
+        message.setSender(user2);
+        message.setReceiver(user1);
+        message.setText(mess);
+        message.setId(id);
+        message.setTime(new Timestamp(01));
+
+        dbs.addMessage(message);
+
+        List<Message> spMess = dbs.getChat(message.getSender(), message.getReceiver());
+        Message tmpMess = null;
+        Boolean found = false;
+        for (int i = 0; i<spMess.size(); i++) {
+            tmpMess = spMess.get(i);
+            if ((message.getId().equals(tmpMess.getId())) & (message.getText().equals(tmpMess.getText()))) found = true;
+        }
+        return found;
     }
 }
