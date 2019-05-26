e4c2532f5ec4633d8a28fc240d027d8662952334
PaLoMaster <renat.programmizm@yandex.ru>
2019-05-27 02:25:40 +0400

    HibernateUtilTest: add setFieldsOverReflection (prepare for test)

--- src/test/java/ru/geekbrains/pocket/messenger/database/HibernateUtilTest.java
+++ src/test/java/ru/geekbrains/pocket/messenger/database/HibernateUtilTest.java
@@ -3,37 +3,53 @@ package ru.geekbrains.pocket.messenger.database;
 import org.junit.Before;
 import org.junit.Test;
 import java.io.File;
-
-
+import java.lang.reflect.Field;
+import org.apache.logging.log4j.LogManager;
 
 public class HibernateUtilTest {
 
-    private static final String WORK_DIR = System.getProperty("user.dir");
-    private static final String DATABASE_DIR = "database";
-    private static String username = "defaultUser";
-    private File TestFile = new File(WORK_DIR + File.separator + DATABASE_DIR + File.separator + username + ".db");
+    private static String WORK_DIR;
+    private static String DATABASE_DIR;
+    private static String username;
+    private File testFile;
 
-
-
+    @Before
+    public void setFieldsOverReflection() {
+        try {
+            HibernateUtil hibUtil = new HibernateUtil();
+            Field temp = hibUtil.getClass().getDeclaredField("WORK_DIR");
+            temp.setAccessible(true);
+            WORK_DIR = (String) temp.get(hibUtil);
+            temp = hibUtil.getClass().getDeclaredField("DATABASE_DIR");
+            temp.setAccessible(true);
+            DATABASE_DIR = (String) temp.get(hibUtil);
+            temp = hibUtil.getClass().getDeclaredField("username");
+            temp.setAccessible(true);
+            username = "t";
+            temp.set(hibUtil, username);
+            testFile = new File(WORK_DIR + File.separator + DATABASE_DIR + File.separator + username + ".db");
+        } catch (IllegalAccessException | IllegalArgumentException | NoSuchFieldException | SecurityException ex) {
+            LogManager.getLogger(getClass().getName()).error(ex);
+        }
+    }
 
     @Test
     public void getSessionFactoryTest() {
-        if(TestFile.exists()){
+        if(testFile.exists()){
             HibernateUtil.deleteDBFile();
         }
-        assert(!TestFile.exists());
+        assert(!testFile.exists());
         HibernateUtil.getSessionFactory();
-        assert(TestFile.exists());
+        assert(testFile.exists());
     }
-
 
     @Test
     public void deleteDBFileTest() {
-        if(!TestFile.exists()){
+        if(!testFile.exists()){
             HibernateUtil.getSessionFactory();
         }
-        assert(TestFile.exists());
+        assert(testFile.exists());
         HibernateUtil.deleteDBFile();
-        assert(!TestFile.exists());
+        assert(!testFile.exists());
     }
 }
